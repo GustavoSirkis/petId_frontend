06df0a83405087fef9c3e1ea5222a215b874f4fc
GustavoSirkis <gustavosirkis@gmail.com>
2024-07-19 20:44:18 -0300

    config baseUrl

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+import.meta.env.BASE_URL
+
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
